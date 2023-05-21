2b11a4d468b5406d98425f5190c3bcca2c266456
isabellexvr <isabellexavier59@gmail.com>
2023-05-21 20:48:16 -0300

    initial configs

---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from './dtos/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+    constructor(private usersService: UsersService) { }
+
+    @Post("sign-up")
+    async createUser(@Body() body: CreateUserDTO) {
+        return await this.usersService
+    }
+}
